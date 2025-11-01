bb41b97edd209731864766b56bc03913fd596adf
Foelli <159255468+Foelli@users.noreply.github.com>
2025-11-01 21:11:43 +0100

    chore(setup): configure angular 19 routing and fix routerLink imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive]  // ⬅️ wichtig
 })
-export class AppComponent {
-  title = 'portfolio';
-}
+export class AppComponent { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: '', component: AboutComponent },
+    { path: '', component: ProjectsComponent },
+    { path: '**', redirectTo: '' }
+
+]
